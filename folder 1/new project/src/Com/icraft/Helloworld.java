b8e4d740f4d51e9c4948404c12da38e83dd2f1f3
yaredadane <86116037+yaredadane@users.noreply.github.com>
2021-06-23 12:54:01 -0700

    su,
    jsdkjf

---
+++ folder 1/new project/src/Com/icraft/Helloworld.java
@@ -0,0 +1,13 @@
+package Com.icraft;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helloworld from b21");
+		
+		
+
+	}
+
+}
